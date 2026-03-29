664f9f4d578ed937d18517cecf18ced8abff8288
Sahin Baran Oztugral <sahinbaranoztugral@Mac.home>
2026-03-29 17:32:32 +0300

    chore: Force rebuild to clear CI cache

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,5 +1,6 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 
+// Force rebuild - 5e842ad cleanup
 export default defineCloudflareConfig({
   edgeExternals: ["node:crypto"],
 });
